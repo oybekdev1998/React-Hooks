05d2a9274ebc22d7e5abe7136a886cc23c604f3e
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-02-18 15:32:38 +0500

    Set Search component

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Profile } from './pages/Profile'
 import { Alert } from './components/Alert';
+import { AlertState } from './components/context/alert/AlertState';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4 App">
-        <Alert alert={{text: 'Test alert'}} />
-        <Routes>
-          <Route path='/' exact="true" element={<Home />} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/profile:name' element={<Profile/>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4 App">
+          <Alert alert={{text: 'Test alert'}} />
+          <Routes>
+            <Route path='/' exact="true" element={<Home />} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/profile:name' element={<Profile/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,16 @@
-import React from "react"
+import React, {useContext} from "react"
+import { AlertContext } from "./context/alert/alertContext"
 
-export const Alert = ({alert}) => {
+export const Alert = () => {
+  const {alert, hide} = useContext(AlertContext)
 
+  if(!alert) return null
   return (
     <div className={`alert alert-${alert.type || 'secondary'} alert-dismissible`} 
     role="alert"
     >
       {alert.text}
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={hide}></button>
     </div>  
   )
 }
